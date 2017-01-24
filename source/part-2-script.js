850bf9fd0c55ece582498191183dee4f4689f5d6
Martin Kleppe <kleppe@ubilabs.net>
2017-01-24 20:33:25 +0100

    split source

---
+++ source/part-2-script.js
@@ -0,0 +1,12 @@
+html * { color: purple }                               
+html * { background: url()}                            
+document.write("<"+"link href=# rel=stylesheet>");     
+var html, purple;                                      
+function url(){};
+                                     
+/**                                                    
+  <h1>Polyglot Inception</h1>                          
+
+
+<h2>HTML - JS - CSS - JPEG</h2>                      
+  <script charset="ISO-8859-1" src=#></script>
